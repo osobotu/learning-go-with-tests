f8c2be5fb9836431e4eb863514cfd89d21cdee62
Stephen Obotu Oduh <osobotu@gmail.com>
2022-12-30 22:29:59 +0100

    Completed Hello World section

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,10 +2,37 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const idomaHelloPrefix = "Nmaochi, "
+const spanish = "Spanish"
+const french = "French"
+const idoma = "Idoma"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case idoma:
+		prefix = idomaHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,9 +3,40 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Mbappe", "French")
+		want := "Bonjour, Mbappe"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Idoma", func(t *testing.T) {
+		got := Hello("Oche", "Idoma")
+		want := "Nmaochi, Oche"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
