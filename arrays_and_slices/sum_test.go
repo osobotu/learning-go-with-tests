0479f0eb5490310cbf17da9ca709089c1afe214e
Stephen Obotu Oduh <osobotu@gmail.com>
2022-12-31 03:09:56 +0100

    completed arrays and slices section

---
+++ arrays_and_slices/sum_test.go
@@ -0,0 +1,65 @@
+package arrays_and_slices
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	t.Run("get the sum of all the passed slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		assertEqualSlices(t, got, want)
+
+	})
+
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	// This is to demonstrate assigning a function to a variable
+	assertEqualSlices := func(t testing.TB, got []int, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of the tails of all slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		assertEqualSlices(t, got, want)
+	})
+
+}
+
+func assertEqualSlices(t testing.TB, got []int, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
